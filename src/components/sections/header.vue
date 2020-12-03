f39a47f57f964553e8cae9dbe88ca4d981327a4b
miladMohammadi <afraaliansite@gmail.com>
2020-12-03 05:54:21 -0800

    commit 10

--- src/components/sections/header.vue
+++ src/components/sections/header.vue
@@ -87,15 +87,14 @@
 
             <div class="menu-user" :class="{ showUser:menuOpenBoxUser }">
                 <ul>
-                    <li class="user-link"><a href="#"><i class="fas fa-angle-down"></i>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;ناحیه کاربری</a>
+                    <li class="user-link"><a href="#"><i class="fas fa-angle-down"></i>ناحیه کاربری</a>
                         <div class="dropdown-user" >
                             <ul class="" >
-                                <li style="background: #ffce46;"><a href="">ورود&emsp;<i class="fas fa-sign-in-alt"></i></a></li>
+                                <li style="background: #ffce46;"><a href="">ورود<i class="fas fa-sign-in-alt"></i></a></li>
                                 <li style="background: #ffce46;"><a href="">ثبت نام</a></li>
                             </ul>
                         </div>
                     </li>
-                    <li></li>
                 </ul>
             </div>
 
